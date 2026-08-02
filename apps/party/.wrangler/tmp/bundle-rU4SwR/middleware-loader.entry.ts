ba64cc2b8dd6931e968a6efc789c8d7127df3a33
Pol Gubau Amores <gubaupol@gmail.com>
2026-08-02 17:51:53 +0200

    Add new chef card image for Chef Galvez

--- apps/party/.wrangler/tmp/bundle-rU4SwR/middleware-loader.entry.ts
+++ apps/party/.wrangler/tmp/bundle-rU4SwR/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-V0PXPT\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-rU4SwR\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\node_modules\\.pnpm\\wrangler@4.116.0_@cloudflare+workers-types@5.20260730.1\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-V0PXPT\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-rU4SwR\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-V0PXPT\\middleware-insertion-facade.js";
+export * from "C:\\Users\\gubau\\dev\\otros\\victor\\duelo-de-lasañas\\apps\\party\\.wrangler\\tmp\\bundle-rU4SwR\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
